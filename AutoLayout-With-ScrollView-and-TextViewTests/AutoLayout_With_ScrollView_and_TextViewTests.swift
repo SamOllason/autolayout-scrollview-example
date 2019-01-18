ed1e8897b1f2d84b5964a9538fee654bde0fc672
Sam Ollason <Sam_ollason@hotmail.co.uk>
2019-01-18 12:40:26 +0000

    Initial Commit

---
+++ AutoLayout-With-ScrollView-and-TextViewTests/AutoLayout_With_ScrollView_and_TextViewTests.swift
@@ -0,0 +1,34 @@
+//
+//  AutoLayout_With_ScrollView_and_TextViewTests.swift
+//  AutoLayout-With-ScrollView-and-TextViewTests
+//
+//  Created by Samuel Ollason on 18/01/2019.
+//  Copyright © 2019 Samuel Ollason. All rights reserved.
+//
+
+import XCTest
+@testable import AutoLayout_With_ScrollView_and_TextView
+
+class AutoLayout_With_ScrollView_and_TextViewTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
